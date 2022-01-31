576d93abb293cfcab1daab6496ebd291556c1849
saikatsingha <saikatsingha@gmail.com>
2022-01-31 20:48:18 +0530

    Views.py is working

--- compare_products/mobiles/views.py
+++ compare_products/mobiles/views.py
@@ -4,16 +4,16 @@ from mobiles.models import Mobiles
 from mobiles.serializers import Mobiles_Serializers
 
 
-# Create your views here.
+Create your views here.
 
-# class MobileList(generics.ListAPIView):
-#     queryset = Mobiles.objects.all()
-#     serializer_class = Mobiles_Serializers
-#
-#
-# class MobileDetails(generics.RetrieveAPIView):
-#     queryset = Mobiles.objects.all()
-#     serializer_class = Mobiles_Serializers
+class MobileList(generics.ListAPIView):
+    queryset = Mobiles.objects.all()
+    serializer_class = Mobiles_Serializers
+
+
+class MobileDetails(generics.RetrieveAPIView):
+    queryset = Mobiles.objects.all()
+    serializer_class = Mobiles_Serializers
 
 
 
