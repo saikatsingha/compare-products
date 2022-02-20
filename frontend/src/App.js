24fb757b2be6c3d95d3828eba0275f6f60aeb5c3
saikatsingha <saikatsingha@gmail.com>
2022-02-20 23:29:30 +0530

    rebuilding react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-import {Mobiles} from "./components/Mobiles";
-import {MobileDetail} from "./components/MobileDetail";
-import {Home} from "./components/Home";
-import {Navigation} from "./components/Navigation";
+// import {Mobiles} from "./components/Mobiles";
+// import {MobileDetail} from "./components/MobileDetail";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 
-
-
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
 
        <Navigation/>
          
         <Routes>
-          <Route path="/" element={<Home />} exact/>
+          <Route exact path='/' component={Home} ></Route>
           <Route path="/mobiles" element={<Mobiles />} />
           <Route path="/mobiles/:id" element={<MobileDetail />} />
         </Routes>
