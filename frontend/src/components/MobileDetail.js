ba9940be94a12152d74046bded3afa764ae8bdf3
saikatsingha <saikatsingha@gmail.com>
2022-02-23 21:00:07 +0530

    Mobile details data is showing using console log

--- frontend/src/components/MobileDetail.js
+++ frontend/src/components/MobileDetail.js
@@ -3,18 +3,26 @@ import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const MobileDetail = (props) => {
-    let params = useParams();
-    //console.log(params);
-    // const [mobile, setMobile] = useState({});
+        let params = useParams();        
+        const [mobile, setMobile] = useState({});
+        useEffect(() => {      
+                const fetchData = async () => {
+                    try {
+                        const res = await axios.get(`http://127.0.0.1:8000/mobiles/${params.id}`);
+                        setMobile(res.data);
+                    }
+                    catch (err) {        
+                    }
+                };
+        
+                fetchData();
 
-    
-            const res = axios.get(`http://127.0.0.1:8000/mobiles/${params.id}`);
-            
+            }, []);
     
     return(
         
             <div classNameName='container mt-3'>
-                {console.log(res.catch.)}
+                {console.log(mobile.company_name)}
                 {/* <h1 classNameName='display-2'>{mobile.title}</h1>
                 <h4>{mobile.month} {mobile.day}</h4>
                 <hr />
