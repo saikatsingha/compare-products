45e579b86457bce64fe7399c1d4c223f08f652f0
saikatsingha <saikatsingha@gmail.com>
2023-03-24 22:13:22 +0530

    added new app mobile and new db with table

--- compare_products/compare_products/settings.py
+++ compare_products/compare_products/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mobile',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,22 @@ WSGI_APPLICATION = 'compare_products.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'compare_products',
+       'USER': 'saikat123',
+       'PASSWORD': '1234',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
 }
 
 
